ed3ebb42c79e1017bf4ef39a98f47658a5b26a91
dasyx <seanpnn00@gmail.com>
2024-09-25 23:45:46 +0200

    add new fonts + add categories title

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,26 @@ import { Component } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
   standalone: true, // Assurez-vous que votre composant est bien défini comme "standalone"
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule,
+    NgIf,
+  ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
